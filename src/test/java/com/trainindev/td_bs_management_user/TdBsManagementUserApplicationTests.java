dabd179b026fc54b0df017f54549ef78185dbaeb
jung <jcy0011@yahoo.com>
2024-08-20 13:54:39 +0900

    init spring boot

---
+++ src/test/java/com/trainindev/td_bs_management_user/TdBsManagementUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainindev.td_bs_management_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdBsManagementUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
